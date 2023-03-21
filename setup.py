cc50b9ff1f54fc23bc9f9c9ddf666be56b07b313
Nickleha <67520178+Nickleha@users.noreply.github.com>
2023-03-21 12:54:21 -0500

    check user args effect
    Signed-off-by: Nickleha <67520178+Nickleha@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -35,12 +35,12 @@ pkgs = find_packages(exclude=["ts_scripts", "test"])
 build_frontend_command = {
     "Windows": ".\\frontend\\gradlew.bat -p frontend clean assemble",
     "Darwin": "frontend/gradlew -p frontend clean assemble",
-    "Linux": "frontend/gradlew -p frontend clean assemble --scan",
+    "Linux": "frontend/gradlew -p frontend clean assemble",
 }
 build_plugins_command = {
     "Windows": ".\\plugins\\gradlew.bat -p plugins clean bS",
     "Darwin": "plugins/gradlew -p plugins clean bS",
-    "Linux": "plugins/gradlew -p plugins clean bS",
+    "Linux": "plugins/gradlew -p plugins clean bS --scan",
 }
 
 
@@ -117,12 +117,12 @@ class BuildPlugins(Command):
     source_plugin_dir = os.path.abspath("plugins/build/plugins")
 
     def initialize_options(self):
-        self.plugins = None
+        self.plugins = "endpoints"
 
     def finalize_options(self):
         if self.plugins is None:
             print("No plugin option provided. Defaulting to 'default'")
-            self.plugins = "default"
+            self.plugins = "plugin"
 
     # noinspection PyMethodMayBeStatic
     def run(self):
